6b60766d2d819005f7e6befebd61161032365430
Willian Classen <Wgclassen@gmail.com>
2025-08-20 15:00:38 -0300

    Variables

--- NumberGuesser/NumberGuesser.c
+++ NumberGuesser/NumberGuesser.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main () {
-    printf("Welcome to number guesser");
+    printf("*****************************\n");
+    printf("* Welcome to number guesser *\n");
+    printf("*****************************\n");
+
+    int secretNumber = 42;
+
+    int guess;
+
+    printf("Guess a number: ");
+    scanf("%d", &guess);
+    printf("\nYour guess was %d", guess);
 }
+
