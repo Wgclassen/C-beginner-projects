9cedf51bca90c51aa761f8ba886e76665b8a1613
Willian Classen <Wgclassen@gmail.com>
2025-08-24 08:59:49 -0300

    Gameplay loop

--- NumberGuesser/NumberGuesser.c
+++ NumberGuesser/NumberGuesser.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 
-int main () {
+int main()
+{
     printf("*****************************\n");
     printf("* Welcome to number guesser *\n");
     printf("*****************************\n");
 
     int secretNumber = 42;
     int guess;
+    int guessNumber = 0;
 
-    printf("Guess a number: ");
-    scanf("%d", &guess);
-    printf("\nYour guess was %d", guess);
-
-    int success = (guess == secretNumber);
-
-    if(success) {
-        printf("\nCongratulations! You found the secret number!");
-    } else {
-        if (guess > secretNumber){
+    while (1)
+    {
+        guessNumber++;
+        printf("\nGUESS #%d\n", guessNumber);
+        printf("Guess a number: ");
+        scanf("%d", &guess);
+    
+        if(guess < 0) {
+            printf("You can't guess negative numbers!\n");;
+            continue;
+        }
+    
+        int success = (guess == secretNumber);
+        int higher = guess > secretNumber;
+        int lower = guess < secretNumber;
+    
+        printf("\nYour guess was %d", guess);
+    
+        if (success)
+        {
+            printf("\nCongratulations! You found the secret number in %d guesses!", guessNumber);
+            break;
+        }
+        else if (higher)
+        {
             printf("\nYour pick is higher than the secret number, try again!");
-        } else {
+        }
+        else
+        {
             printf("\nYour pick is lower than the secret number, try again!");
         }
     }
 }
-
