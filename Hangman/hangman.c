a02f5a641c207edaab88fe1ec50796d85e981fc9
Willian Classen <Wgclassen@gmail.com>
2025-08-30 11:45:12 -0300

    Hangman gameplay loop

--- Hangman/hangman.c
+++ Hangman/hangman.c
@@ -1,26 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     char secretWord[20];
 
     sprintf(secretWord, "WATERMELON APPLE");
 
-    printf("%s", secretWord);
-
     int success = 0;
     int hanged = 0;
+    char maxAttempts[26];
+    int tries = 0;
 
-    do {
-        char guess;
-        scanf("%c", &guess);
-
+    do
+    {
         for (int i = 0; i < strlen(secretWord); i++)
         {
-            if(secretWord[i] == guess) {
-                printf("The position %d has this letter\n", i);
+            int found = 0;
+
+            for (int j = 0; j < tries; j++)
+            {
+                if (maxAttempts[j] == secretWord[i])
+                {
+                    found = 1;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                printf("%c ", secretWord[i]);
+            }
+            else
+            {
+                printf("_ ");
             }
         }
-        
+        printf("\n");
+
+        char guess;
+        scanf(" %c", &guess);
+
+        maxAttempts[tries] = guess;
+        tries++;
+
     } while (!success && !hanged);
 }
