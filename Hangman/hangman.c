1e7e155e2edd47625d1d7524549445f91b900705
Willian Classen <Wgclassen@gmail.com>
2025-09-11 19:38:53 -0300

    Hangman finishing details

--- Hangman/hangman.c
+++ Hangman/hangman.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "hangman.h"
 
-char secretWord[20];
+char secretWord[TAMANHO_PALAVRA];
 char guesses[26];
 int attempts = 0;
 
@@ -42,6 +43,20 @@ int main()
         drawGallows();
         guess();
     } while (!victory() && !hanged());
+
+    if (victory())
+    {
+        printf("\nCONGRATULATIONS!! YOU FOUND THE SECRET WORD!!");
+    }
+    else
+    {
+        printf("\nOH NO, YOU'VE BEEN HANGED!!\nTHE SECRET WORD WAS: ");
+        for (int i = 0; i < strlen(secretWord); i++)
+        {
+            printf("%c", toupper(secretWord[i]));
+        }
+    }
+
     addWord();
 }
 
@@ -55,7 +70,19 @@ void title()
 void guess()
 {
     char guess;
+    printf("Guess a letter: ");
     scanf(" %c", &guess);
+
+    guess = toupper(guess);
+
+if (charExists(guess))
+{
+    printf("\nCorrect: the word has the letter %c\n\n", guess);
+} else {
+    printf("\nWrong: the word does NOT have the letter %c\n\n", guess);
+}
+
+
     guesses[attempts] = guess;
     attempts++;
 }
@@ -65,7 +92,7 @@ int alreadyGuessed(char letter)
     int found = 0;
     for (int j = 0; j < attempts; j++)
     {
-        if (guesses[j] == letter)
+        if (guesses[j] == toupper(letter))
         {
             found = 1;
             break;
@@ -76,13 +103,28 @@ int alreadyGuessed(char letter)
 
 void drawGallows()
 {
+    int mistakes = countMistakes();
+
+    printf("  _______       \n");
+    printf(" |/      |      \n");
+    printf(" |      %c%c%c  \n", (mistakes >= 1 ? '(' : ' '),
+           (mistakes >= 1 ? '_' : ' '), (mistakes >= 1 ? ')' : ' '));
+    printf(" |      %c%c%c  \n", (mistakes >= 3 ? '\\' : ' '),
+           (mistakes >= 2 ? '|' : ' '), (mistakes >= 3 ? '/' : ' '));
+    printf(" |       %c     \n", (mistakes >= 2 ? '|' : ' '));
+    printf(" |      %c %c   \n", (mistakes >= 4 ? '/' : ' '),
+           (mistakes >= 4 ? '\\' : ' '));
+    printf(" |              \n");
+    printf("_|___           \n");
+    printf("\n\n");
+
     for (int i = 0; i < strlen(secretWord); i++)
     {
         int found = alreadyGuessed(secretWord[i]);
 
         if (found)
         {
-            printf("%c ", secretWord[i]);
+            printf("%c ", toupper(secretWord[i]));
         }
         else
         {
@@ -92,25 +134,32 @@ void drawGallows()
     printf("\n");
 }
 
-int hanged()
+int charExists(char character) {
+
+    for (int j = 0; j < strlen(secretWord); j++)
+    {
+        if (character == toupper(secretWord[j]))
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int countMistakes()
 {
     int mistakes = 0;
     for (int i = 0; i < attempts; i++)
     {
-        int exists = 0;
-
-        for (int j = 0; j < strlen(secretWord); j++)
-        {
-            if (guesses[i] == secretWord[j])
-            {
-                exists = 1;
-                break;
-            }
-        }
-        if (!exists)
+        if (!charExists(guesses[i]))
             mistakes++;
     }
-    return mistakes >= 5;
+    return mistakes;
+}
+
+int hanged()
+{
+    return countMistakes() >= 5;
 }
 
 int victory()
@@ -129,12 +178,14 @@ void addWord()
 {
 
     char option;
-    printf("Would you like to add a new word to the game? (Y/N)");
+    printf("\nWould you like to add a new word to the game? (Y/N)");
     scanf(" %c", &option);
+
+    option = toupper(option);
 
     if (option == 'Y')
     {
-        char newWord[20];
+        char newWord[TAMANHO_PALAVRA];
         printf("Type your new word: ");
         scanf("%s", newWord);
 
--- Hangman/hangman.h
+++ Hangman/hangman.h
@@ -1,7 +1,10 @@
+#define TAMANHO_PALAVRA 20
 void title();
 void guess();
 int alreadyGuessed(char letter);
 int hanged();
+int countMistakes();
 int victory();
 void drawGallows();
 void addWord();
+int charExists(char character);
