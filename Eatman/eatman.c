df6e446e8907516c5a68593512aeb2c1ce7e95cc
Willian Classen <Wgclassen@gmail.com>
2025-09-19 21:53:56 -0300

    Dynamic map size

--- Eatman/eatman.c
+++ Eatman/eatman.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+char** map;
 
-int main() {
-    char map[5][10+1];
+int rows;
+int cols;
+int main()
+{
 
-    FILE* f;
+    FILE *f;
     f = fopen("map.txt", "r");
-    if(f == 0) {
+    if (f == 0)
+    {
         printf("Error when reading map\n");
         exit(1);
     }
 
-    for(int i = 0; i < 5; i++) {
+    fscanf(f, "%d %d", &rows, &cols);
+
+    map = malloc(sizeof(char *) * rows);
+    for (int i = 0; i < rows; i++)
+    {
+        map[i] = malloc(sizeof(char) * (cols + 1));
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
         fscanf(f, "%s", map[i]);
     }
 
-    for(int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         printf("%s\n", map[i]);
     }
 
     fclose(f);
+    for (int i = 0; i < rows; i++)
+    {
+        free(map[i]);
+    }
+    free(map);
 }
